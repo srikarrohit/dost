6137e2bb46bb2674a5f511e8f8a9c8223b709a23
Shashank S <shashank1693@gmail.com>
2015-02-16 21:32:58 +0530

    title changed

--- saved.php
+++ saved.php
@@ -5,7 +5,7 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 	     
-	    <title>Welcome!</title>
+	    <title>The Dean's Blog | Saved Posts</title>
 	 
 	    <!-- Bootstrap -->
 	    <!-- Latest compiled and minified CSS -->
